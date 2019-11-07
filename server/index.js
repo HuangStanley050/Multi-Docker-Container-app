3b48de898c6fc219ce450d51c7063e891f0c35c1
Stanley Huang <huang.stanley050@gmail.com>
2019-11-07 19:22:58 +1100

    api finished

--- server/index.js
+++ server/index.js
@@ -29,16 +29,29 @@ const redisClient = redis.createClient({
 const redisPublisher = redisClient.duplicate();
 
 app.get("/", (req, res) => {
-  res.send("Hi");
+  return res.send("Hi");
 });
 
 app.get("/values/all", async (req, res) => {
   const values = await pgClient.query("SELECT * FROM values");
-  res.send(values.rows);
+  return res.send(values.rows);
 });
 
 app.get("/values/current", async (req, res) => {
   redisClient.hgetall("values", (err, values) => {
-    res.send(values);
+    return res.send(values);
   });
 });
+
+app.post("/values", async (req, res) => {
+  const index = req.body.index;
+  if (parseInt(index) > 40) {
+    res.status(422).send("value too high!!");
+  }
+  redisClient.hset("values", index, "nothing yet");
+  redisPublisher.publish("insert", index);
+  pgClient.query("INSERT INTO values(number) VALUES($1)", [index]);
+  return res.send({ working: true });
+});
+
+app.listen(5000, () => console.log("server running on port 5000"));
